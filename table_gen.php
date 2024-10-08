86a7fab5429f6d7834a47414db9ea77a589ef32d
F313-055\user <d1114182008@gm.lhu.edu.tw>
2024-10-08 11:56:40 +0800

    no message

--- table_gen.php
+++ table_gen.php
@@ -2,9 +2,13 @@
     $rowspan = $_POST['row_num'];
     $colspan = $_POST['col_num'];
     $gender = $_POST['gender'];
+    $dept = $_POST['dept'];
+    $msg = $_POST['message'];
 
     echo'表格大小: '. $rowspan . '列, ' . $colspan. '行'; 
-    echo'性別: '. $gender ;
+    echo'性別: '. $gender . ',' ;
+    echo'科系: '. $dept . ',';
+    echo'留言: '. $msg ;
     
     //table
     echo '<table border="1" style="width: 100%">';
